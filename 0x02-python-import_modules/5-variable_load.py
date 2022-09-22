e58e00eb1d9f91f0e52ffbd5807843a0e1af5839
PilotVader <pilotsamson@gmail.com>
2022-09-22 15:58:41 -0700

    0x02-python-import_modules

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-import hidden_4 as h
+from variable_load_5 import a
 
 if __name__ == '__main__':
-    names = filter(lambda n: not n.startswith('__'), dir(h))
-    for name in names:
-        print(name)
+    print(a)
