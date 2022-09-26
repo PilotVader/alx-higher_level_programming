ebe47819c64d323e355a9b65bfbcfeff6ac3bdc1
PilotVader <pilotsamson@gmail.com>
2022-09-26 14:42:32 -0700

    0x03-python-data_structures

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    """
+    deletes an elment from a list at index idx
+    Args:
+        my_list - list to search
+        idx - the position to access
+    Return:
+        my_list - if idx is out of range
+    """
+    if -1 < idx < len(my_list):
+        del my_list[idx]
+    return my_list
+
+
+if __name__ == '__main__':
+    lst = list(range(1, 6))
+    tmp = delete_at(lst, 3)
+    print(lst)
+    print(tmp)
